02ff5fcf9ec9c8213d045b0f9dd7a85b93e54436
Renan Queiroz <queirozrrq@gmail.com>
2019-12-05 00:40:50 -0500

    TONS of changes and improvements

--- projects/dodge/game.js
+++ projects/dodge/game.js
@@ -1,140 +1,137 @@
 var player;
-var level;
-var enemy;
-var enemy2;
-var enemy3;
-var enemy4;
-var enemy5;
-var levelWasCleared;
+var enemies;
+var enemyQty;
 var isGameOver;
+var score;
+var firstRun;
+var playerAccelFactor = 8;
 
 function setup() {
-  levelWasCleared = false;
-  isGameOver = true;
-  createCanvas(500, 500);
-  clearedLevel = createSprite (2, 0, width*2, 100);
-  player = createSprite(width/2, height-25, 50, 50);
-  enemy = createSprite(width/2, 0, 10, 30);
-  enemy2 = createSprite(width/2, 0, 10, 30);
-  enemy3 = createSprite(width/2, 0, 10, 30);
-  enemy4 = createSprite(width/2, 0, 10, 30);
-  enemy5 = createSprite(width/2, 0, 10, 30);
+	score = 0;
+	isGameOver = true;
+	createCanvas(500, 500);
+	player = createSprite(width/2, height-25, 50, 50);
+
+	enemies = [];
+	enemyQty = 5;
+
+	for (i = 0; i < enemyQty; i++) {
+		var enemy = createSprite(width/2, 0, 10, 30);
+		enemies.push(enemy);
+	}
 }
 
 function draw() {
+	console.log(score);
 
-    if (isGameOver || levelWasCleared) {
+	if(firstRun){
+		for (i = 0; i < enemyQty; i++) {
+			enemies[i].position.y = 0;
+			enemies[i].position.x = random(5, width-5);
+		}
+		firstRun = false;
+	}
+
+	if (isGameOver) {
         gameOver();
-    } else {
-      if (player.overlap(clearedLevel) || clearedLevel.overlap(player)) {
-        levelWasCleared = true;
-        gameOver();
-      } else if (enemy.overlap(player) || enemy2.overlap(player) || enemy3.overlap(player) || enemy4.overlap(player) || enemy5.overlap(player)){
-          isGameOver = true;
-          gameOver();
-        }
+    } 
+    else {
+		for (i = 0; i < enemyQty; i++) {
+			if(enemies[i].overlap(player)){
+				isGameOver = true;
+				gameOver();
+			}
+		}
+
+		background(0, 0, 100);
+		fill("white");
+		textSize(32);
+		textAlign(CENTER, CENTER);
+		text(Math.floor(score/10), width / 2, 64);
+
+		if (keyDown(RIGHT_ARROW) && player.position.x < (width-25)) {
+			player.position.x = player.position.x + playerAccelFactor;
+		}
+		if (keyDown(LEFT_ARROW) && player.position.x > 25) {
+			player.position.x = player.position.x - playerAccelFactor;
+		}
+		if (keyDown(DOWN_ARROW) && player.position.y < (height-25)) {
+			player.position.y = player.position.y + playerAccelFactor;
+		}
+		if (keyDown(UP_ARROW) && player.position.y > 25) {
+			player.position.y = player.position.y - playerAccelFactor;
+		}
+
+		if (keyDown(68) && player.position.x < (width-25)) {
+			 player.position.x = player.position.x + playerAccelFactor;
+		}
+		if (keyDown(65) && player.position.x > 25) {
+			player.position.x = player.position.x - playerAccelFactor;
+		}
+		if (keyDown(83) && player.position.y < (height-25)) {
+			player.position.y = player.position.y + playerAccelFactor;
+		}
+		if (keyDown(87) && player.position.y > 25) {
+			player.position.y = player.position.y - playerAccelFactor;
+		}
+
+		posFactor = 5 + Math.round(Math.sqrt(score)/5);
+
+		for (i = 0; i < enemyQty; i++) {
+			enemies[i].position.y = enemies[i].position.y + posFactor;
+			posFactor++;
+		}
 
 
-  background(0, 0, 100);
+		for (i = 0; i < enemyQty; i++) {
+			if (enemies[i].position.y > height) {
+				score++;
+				enemies[i].position.y = 0;
+				enemies[i].position.x = random(5, width-5);
+			}
+		}
 
-  if (keyDown(RIGHT_ARROW) && player.position.x < (width-25)) {
-    player.position.x = player.position.x + 4;
-}
-  if (keyDown(LEFT_ARROW) && player.position.x > 25) {
-    player.position.x = player.position.x - 4;
-}
-  if (keyDown(DOWN_ARROW) && player.position.y < (height-25)) {
-    player.position.y = player.position.y + 4;
-}
-  if (keyDown(UP_ARROW) && player.position.y > 25) {
-    player.position.y = player.position.y - 4;
-}
-
-
-if (keyDown(68) && player.position.x < (width-25)) {
-    player.position.x = player.position.x + 4;
-}
-  if (keyDown(65) && player.position.x > 25) {
-    player.position.x = player.position.x - 4;
-}
-  if (keyDown(83) && player.position.y < (height-25)) {
-    player.position.y = player.position.y + 4;
-}
-  if (keyDown(87) && player.position.y > 25) {
-    player.position.y = player.position.y - 4;
-}
-
-
-  enemy.position.y = enemy.position.y + 8;
-  enemy2.position.y = enemy2.position.y + 9;
-  enemy3.position.y = enemy3.position.y + 10;
-  enemy4.position.y = enemy4.position.y + 11;
-  enemy5.position.y = enemy5.position.y + 12;
-
-if (enemy.position.y > height) {
-  enemy.position.y = 0;
-  enemy.position.x = random(5, width-5);
-}
-if (enemy2.position.y > height) {
-  enemy2.position.y = 0;
-  enemy2.position.x = random(5, width-5);
-}
-if (enemy3.position.y > height) {
-  enemy3.position.y = 0;
-  enemy3.position.x = random(5, width-5);
-}
-if (enemy4.position.y > height) {
-  enemy4.position.y = 0;
-  enemy4.position.x = random(5, width-5);
-}
-if (enemy5.position.y > height) {
-  enemy5.position.y = 0;
-  enemy5.position.x = random(5, width-5);
-}
-
-    drawSprites();
-  }
+		drawSprites();
+	}
 }
 
 function gameOver() {
-  background(0);
-  textAlign(CENTER);
-  fill("white");
-  text("PLAY", width / 2, height / 2);
-
-  if (levelWasCleared) { 
-    text("Congratulations, you beat the game!", width / 2, height / 1.5);
-    text("Click or tap any button to play again!", width / 2, height / 1.4);
-  } else {
-    text("Click or tap any button to start!", width / 2, height / 1.5);
-  }
+	background(0);
+	textAlign(CENTER);
+	fill("white");
+	text("PLAY", width / 2, height / 2);
+	text("Click or tap any button to start!", width / 2, height / 1.5);
 }
 
 function mouseClicked() {
-  if (isGameOver || levelWasCleared) {
-    isGameOver = false;
-    levelWasCleared = false;
-    player.position.x = width/4;
-    player.position.y = height-25;
-    enemy.position.x = width/2;
-    enemy.position.y = 0;
-    enemy2.position.x = width/2;
-    enemy2.position.y = 0;
-    enemy3.position.x = width/2;
-    enemy3.position.y = 0;
-    enemy4.position.x = width/2;
-    enemy4.position.y = 0;
-    enemy5.position.x = width/2;
-    enemy5.position.y = 0;
-  }
-  
+	if (isGameOver){
+		score = 0;
+		firstRun = true;
+		isGameOver = false;
+		player.position.x = width/2;
+		player.position.y = height-25;
+
+		for (i = 0; i < enemyQty; i++) {
+			enemies[i].position.x = width/2;
+			enemies[i].position.y = 0;
+		}
+	}
 }
 
 function keyPressed() {
-  mouseClicked();
+	mouseClicked();
 }
 
 function touchMoved() {
-  	player.position.x = mouseX;
-  	player.position.y = mouseY;
+	var a = player.position.x - mouseX;
+	var b = player.position.y - mouseY;
+
+	var distance = Math.sqrt( a*a + b*b );
+
+	if(distance <= 50){
+		if(mouseX < (width-25) && mouseX > 25 && mouseY < (height-25) && mouseY > 25){
+			player.position.x = mouseX;
+			player.position.y = mouseY;
+		}
+	}
 }

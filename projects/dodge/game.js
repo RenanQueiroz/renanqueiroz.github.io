39572f04bca93bce27142c7c911afdf9361d990f
Renan Queiroz <queirozrrq@gmail.com>
2021-07-09 16:12:25 -0400

    Dodge v1.1
    * Now the game is playable on mobile!

--- projects/dodge/game.js
+++ projects/dodge/game.js
@@ -6,11 +6,30 @@ var isGameOver;
 var score;
 var highScore;
 var firstRun;
-var playerAccelFactor = 8;
+var playerAccelFactor;
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+var spriteScaleFactor;
+var textScaleFactor;
+
+function preventBehavior(e) {
+    e.preventDefault(); 
+};
+
+document.addEventListener("touchmove", preventBehavior, {passive: false});
 
 function setup() {
 	frameRate(60);
+	if(isMobile){
+		spriteScaleFactor = 3;
+		textScaleFactor = 4;
+		movLimScaleFactor = 2;
+	}
+	else{
+		spriteScaleFactor = 1;
+		textScaleFactor = 1;
+		movLimScaleFactor = 1;
+	}
+	playerAccelFactor = 8;
 	score = 0;
 	highScore = 0;
 	isGameOver = true;
@@ -18,28 +37,24 @@ function setup() {
 		cnv = createCanvas(windowWidth, windowHeight);
 	}
 	else{
-		if(windowWidth <= 500){
-			cnv = createCanvas(windowWidth, windowHeight);
-		}
-		else{
-			cnv = createCanvas(500, windowHeight);
-		}
+		cnv = createCanvas(500, windowHeight);
+
 	}
 	cnv.style('display', 'block');
 	centerCanvas();
-	player = createSprite(width/2, height-25, 50, 50);
+	player = createSprite(width/2, height-75, 50*spriteScaleFactor, 50*spriteScaleFactor);
 
 	enemies = [];
 	enemyQty = 5;
 
 	for (i = 0; i < enemyQty; i++) {
-		var enemy = createSprite(width/2, 0, 10, 30);
+		var enemy = createSprite(width/2, 0, 10*spriteScaleFactor, 30*spriteScaleFactor);
 		enemies.push(enemy);
 	}
 }
 
 function draw() {
-	console.log(score);
+	//console.log(score);
 
 	if(firstRun){
 		for (i = 0; i < enemyQty; i++) {
@@ -62,37 +77,24 @@ function draw() {
 
 		background(0, 0, 51);
 		fill("white");
-		textSize(32);
+		textSize(32*textScaleFactor);
 		textAlign(CENTER, CENTER);
-		text(Math.floor(score/10), width / 2, 64);
+		text(Math.floor(score/10), width / 2, 100);
 
-		if (keyDown(RIGHT_ARROW) && player.position.x < (width-25)) {
+		if (keyDown(RIGHT_ARROW) || keyDown(68) && player.position.x < (width-25)) {
 			player.position.x = player.position.x + playerAccelFactor;
 		}
-		if (keyDown(LEFT_ARROW) && player.position.x > 25) {
+		if (keyDown(LEFT_ARROW) || keyDown(65) && player.position.x > 25) {
 			player.position.x = player.position.x - playerAccelFactor;
 		}
-		if (keyDown(DOWN_ARROW) && player.position.y < (height-25)) {
+		if (keyDown(DOWN_ARROW) || keyDown(83) && player.position.y < (height-25)) {
 			player.position.y = player.position.y + playerAccelFactor;
 		}
-		if (keyDown(UP_ARROW) && player.position.y > 25) {
+		if (keyDown(UP_ARROW) || keyDown(87) && player.position.y > 25) {
 			player.position.y = player.position.y - playerAccelFactor;
 		}
 
-		if (keyDown(68) && player.position.x < (width-25)) {
-			 player.position.x = player.position.x + playerAccelFactor;
-		}
-		if (keyDown(65) && player.position.x > 25) {
-			player.position.x = player.position.x - playerAccelFactor;
-		}
-		if (keyDown(83) && player.position.y < (height-25)) {
-			player.position.y = player.position.y + playerAccelFactor;
-		}
-		if (keyDown(87) && player.position.y > 25) {
-			player.position.y = player.position.y - playerAccelFactor;
-		}
-
-		posFactor = 5 + Math.round(Math.sqrt(score)/5);
+		posFactor = (5 + Math.round(Math.sqrt(score)/5))*spriteScaleFactor;
 
 		for (i = 0; i < enemyQty; i++) {
 			enemies[i].position.y = enemies[i].position.y + posFactor;
@@ -117,10 +119,18 @@ function gameOver() {
 		highScore = Math.floor(score/10);
 	}
 	background(0);
+	textSize(16*textScaleFactor);
 	textAlign(CENTER);
 	fill("white");
 	text("PLAY", width / 2, height / 2);
-	text("Click or tap any button to start!", width / 2, height / 1.5);
+	if(isMobile){
+		text("Tap anywhere to start!", width / 2, height / 1.5);
+	}
+	else{
+		text("Click or tap any button to start!", width / 2, height / 1.5);
+	}
+	//text("PixelDensity is " + pixelDensity(), width / 2, height / 1.3);
+	//text("isMobile is " + isMobile, width / 2, height / 1.1);
 	if(highScore != 0){
 		text("Your highest score is: " + highScore, width / 2, height / 1.7);
 	}
@@ -135,7 +145,7 @@ function mouseClicked() {
 		firstRun = true;
 		isGameOver = false;
 		player.position.x = width/2;
-		player.position.y = height-25;
+		player.position.y = height-75;
 
 		for (i = 0; i < enemyQty; i++) {
 			enemies[i].position.x = width/2;
@@ -155,7 +165,7 @@ function touchMoved() {
 	var distance = Math.sqrt( a*a + b*b );
 
 	if(distance <= 50){
-		if(mouseX < (width-25) && mouseX > 25 && mouseY < (height-25) && mouseY > 25){
+		if(mouseX < (width-(25*movLimScaleFactor)) && mouseX > (25*movLimScaleFactor) && mouseY < (height-(25*movLimScaleFactor)) && mouseY > (25*movLimScaleFactor)){
 			player.position.x = mouseX;
 			player.position.y = mouseY;
 		}
@@ -169,7 +179,7 @@ function centerCanvas() {
 }
 
 function windowResized() {
-	if(windowWidth <= 500){
+	if(isMobile){
 		resizeCanvas(windowWidth, windowHeight);
 		centerCanvas();
 	}
